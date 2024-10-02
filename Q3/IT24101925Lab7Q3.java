467d967ca7c2dee3149b6e9c84bff3c496b58d79
Kaviyalan Sivakumar <it24101925@my.sliit.lk>
2024-10-03 03:15:57 +0530

    Add files via upload

---
+++ Q3/IT24101925Lab7Q3.java
@@ -0,0 +1,11 @@
+public class IT24101925Lab7Q3{
+public static void main(String[]args){
+for(int number =1; number<=5; number++){
+System.out.print(number+"-");
+for(int star=1; star<=number; star++){
+System.out.print("*");
+}
+System.out.println();
+}
+}
+}
